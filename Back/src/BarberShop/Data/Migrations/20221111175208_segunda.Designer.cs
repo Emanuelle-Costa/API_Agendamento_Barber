6833fc2d24496635ecf0fec9ce9f1569f9b27bc8
Emanuelle Costa <emanuellemcosta@outlook.com>
2022-11-11 14:54:00 -0300

    nova migration

--- Back/src/BarberShop/Data/Migrations/20221111175208_segunda.Designer.cs
+++ Back/src/BarberShop/Data/Migrations/20221111175208_segunda.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BarberShop.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BarberShop.Data.Migrations
 {
     [DbContext(typeof(ContextoBanco))]
-    partial class ContextoBancoModelSnapshot : ModelSnapshot
+    [Migration("20221111175208_segunda")]
+    partial class segunda
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
